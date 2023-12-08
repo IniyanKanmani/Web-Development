eaf770aec1d8ac9bd272b072b87a7ba859cc0659
IniyanKanmani <kiniyan002@gmail.com>
2023-12-08 12:17:29 +0000

    completed ex 23.3

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+import gerenateSuperhero from "superheros";
+import generateName from "sillyname";
+
+gerenateSuperhero();
+
+var sillyname = generateName();
+console.log(`My Name is ${sillyname}.`);
